c2467451df7e2bd83db2ac5e214ac9e3a5939cc7
Diego Barreiro <diego.barreiro.perez@gmail.com>
2017-03-07 17:16:31 +0100

    Added isX plugin
    See if an image contains porn content

--- config.lua
+++ config.lua
@@ -69,6 +69,7 @@ return {
 		-- MY CUSTOM PLUGINS
 		'apod',
 		'cats',
+		'isX',
 		'ping',
 		'talk',
 		'yoda',
---
+++ plugins/isX.lua
@@ -0,0 +1,57 @@
+local config = require 'config'
+local u = require 'utilities'
+local api = require 'methods'
+local ltn12 = require "ltn12"
+local https = require "ssl.https"
+
+local plugin = {}
+
+local function isempty(s)
+  return s == nil or s == ''
+end
+
+local function request(imageUrl)
+   local api_key = config.mashape_api_key
+   if isempty(api_key) then
+      return nil, 'Configure your Mashape API Key'
+   end
+
+   local api = "https://sphirelabs-advanced-porn-nudity-and-adult-content-detection.p.mashape.com/v1/get/index.php?"
+   local parameters = "&url="..(URL.escape(imageUrl) or "")
+   local url = api..parameters
+   local respbody = {}
+   local headers = {
+      ["X-Mashape-Key"] = api_key,
+      ["Accept"] = "Accept: application/json"
+   }
+   print(url)
+   local body, code, headers, status = https.request{
+      url = url,
+      method = "GET",
+      headers = headers,
+      sink = ltn12.sink.table(respbody),
+      protocol = "tlsv1"
+   }
+   if code ~= 200 then return "", code end
+   local body = table.concat(respbody)
+   return body, code
+end
+
+function plugin.onTextMessage(msg, blocks)
+	if blocks[1] == 'yoda' then
+    if not blocks[2] then
+      api.sendReply(msg, "You have to _send me_ an *Image URL after the command*", true, reply_markup)
+    else
+      api.sendReply(msg, request(blocks[2]), true, reply_markup)
+    end
+  end
+end
+
+plugin.triggers = {
+	onTextMessage = {
+		config.cmd..'(isX) (.*)$',
+    config.cmd..'(isX)$'
+	}
+}
+
+return plugin
