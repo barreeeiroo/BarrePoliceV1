092eb08fdf4887b7b5b5fc90eca89aff9519dc71
Diego Barreiro <diego.barreiro.perez@gmail.com>
2017-03-09 15:35:31 +0100

    Update

--- plugins/isX.lua
+++ plugins/isX.lua
@@ -59,7 +59,7 @@ function plugin.onTextMessage(msg, blocks)
       api.sendChatAction(msg.chat.id, "typing")
       local request_data = request(blocks[2])
       local parse_data = parseData(request_data)
-      api.sendReply(msg, parse_data, true, nil, true)
+      api.sendReply(msg, parse_data)
     end
   end
 end
