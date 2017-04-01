fd5bfe63392b39d669efee022b4d6a9bbb611fcf
Diego Barreiro <diego.barreiro.perez@gmail.com>
2017-04-01 12:33:43 +0200

    Fixed Utilities error

--- utilities.lua
+++ utilities.lua
@@ -3,7 +3,6 @@ local config = require 'config'
 local api = require 'methods'
 local ltn12 = require 'ltn12'
 local HTTPS = require 'ssl.https'
-local rss = require 'plugins.rss'
 
 -- Import JSON manager
 json = (loadfile "./libs/JSON.lua")()
@@ -15,40 +14,6 @@ local utf8 = utf8 or require('lua-utf8')
 -- Functions shared among plugins.
 
 local utilities = {}
-
-function print_r(t)  
-    local print_r_cache={}
-    local function sub_print_r(t,indent)
-        if (print_r_cache[tostring(t)]) then
-            print(indent.."*"..tostring(t))
-        else
-            print_r_cache[tostring(t)]=true
-            if (type(t)=="table") then
-                for pos,val in pairs(t) do
-                    if (type(val)=="table") then
-                        print(indent.."["..pos.."] => "..tostring(t).." {")
-                        sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
-                        print(indent..string.rep(" ",string.len(pos)+6).."}")
-                    elseif (type(val)=="string") then
-                        print(indent.."["..pos..'] => "'..val..'"')
-                    else
-                        print(indent.."["..pos.."] => "..tostring(val))
-                    end
-                end
-            else
-                print(indent..tostring(t))
-            end
-        end
-    end
-    if (type(t)=="table") then
-        print(tostring(t).." {")
-        sub_print_r(t,"  ")
-        print("}")
-    else
-        sub_print_r(t,"  ")
-    end
-    print()
-end
 
 -- Escape markdown for Telegram. This function makes non-clickable usernames,
 -- hashtags, commands, links and emails, if only_markup flag isn't setted.
